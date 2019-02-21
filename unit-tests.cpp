940abe6c01b8bb27ce1c7a4db61f6188f807cd60
Simone Basso <bassosimone@gmail.com>
2019-02-21 15:53:33 +0000

    unit-tests: make sure each run gives a different uuid4

--- unit-tests.cpp
+++ unit-tests.cpp
@@ -6,11 +6,15 @@
 
 #include "mkuuid4.hpp"
 
-TEST_CASE("gen works as expected") {
+TEST_CASE("gen generates a valid UUID4") {
   auto uuid4 = mk::uuid4::gen();
   std::clog << uuid4 << std::endl;
   std::regex expect{
       "^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$"};
   std::smatch smatch;
   REQUIRE(std::regex_match(uuid4, smatch, expect) == true);
+}
+
+TEST_CASE("subsequent gen runs generate different UUID4s") {
+  REQUIRE(mk::uuid4::gen() != mk::uuid4::gen());
 }
