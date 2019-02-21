ade79be80fae394e82c2b268f0ae516124811b1a
Simone Basso <bassosimone@gmail.com>
2019-02-21 15:52:02 +0000

    unit-tests.cpp: add more strict checks

--- unit-tests.cpp
+++ unit-tests.cpp
@@ -2,10 +2,15 @@
 #include "catch.hpp"
 
 #include <iostream>
+#include <regex>
 
 #include "mkuuid4.hpp"
 
-SECTION("gen works as expected") {
+TEST_CASE("gen works as expected") {
   auto uuid4 = mk::uuid4::gen();
   std::clog << uuid4 << std::endl;
+  std::regex expect{
+      "^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$"};
+  std::smatch smatch;
+  REQUIRE(std::regex_match(uuid4, smatch, expect) == true);
 }
